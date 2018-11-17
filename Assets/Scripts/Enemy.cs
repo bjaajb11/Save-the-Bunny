a0eea72563ebb238f98878c2867a3d9adf705556
Bart Anderson <bjaajb11@gmail.com>
2018-11-16 23:22:49 -0600

    Added trails to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     [SerializeField] private float _rotationSpeed;
 
 	private void FixedUpdate () {
 	transform.Rotate(0, 0, _rotationSpeed);	
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            return;
+        }
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
