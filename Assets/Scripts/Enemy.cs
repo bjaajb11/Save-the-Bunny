e460b447a0102e956014002dd8ca1049661e14a5
Bart Anderson <bjaajb11@gmail.com>
2018-11-16 22:35:45 -0600

    Added player movement and spiked enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float _rotationSpeed;
+
+	private void FixedUpdate () {
+	transform.Rotate(0, 0, _rotationSpeed);	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    
+    [SerializeField]private float _moveSpeed = 3f;
+    [SerializeField] private float _xConstraint = 5;
+	private void Awake()
+	{
+	    _rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	private void FixedUpdate()
+	{
+	    Move();
+	}
+
+    private void Move()
+    {
+        var xInput = Input.GetAxis("Horizontal");
+        var position = transform.position;
+        position.x += xInput * _moveSpeed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -_xConstraint, _xConstraint);
+        _rigidbody.MovePosition(position);
+    }
+}
