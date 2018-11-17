5706d3fba02748a8cb114bc64eb5cc562d84b53e
Bart Anderson <bjaajb11@gmail.com>
2018-11-16 22:51:19 -0600

    Added enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _horizontalLimits;
+    [SerializeField] private float _spawnRate;
+
+    private void SpawnSpike()
+    {
+        var position = transform.position;
+        position.x = Random.Range(-_horizontalLimits, _horizontalLimits);
+        Instantiate(_enemyPrefab, position, Quaternion.identity);
+    }
+
+    private void Start()
+    {
+        StartSpawning();
+    }
+
+    private void StartSpawning()
+    {
+        InvokeRepeating("SpawnSpike", 1f, _spawnRate);
+    }
+    private void Stoppawning()
+    {
+        CancelInvoke("SpawnSpike");
+    }
+}
